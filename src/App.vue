b6b69874cec0f61927ee6d93e7fd8ac5f7c2b6d5
Anne <anneris.wong@gmail.com>
2023-09-26 21:25:01 -0400

    First commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import QRCode from "./components/QRCode.vue";
+</script>
+
+<template>
+  <main>
+    <QRCode />
+  </main>
+</template>
---
+++ src/components/QRCode.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div class="qr-wrapper">
+    <img class="qr-img" src="../assets/images/image-qr-code.png" />
+    <h1>Improve your front-end skill by building projects</h1>
+    <p class="small-description">
+      Scan the QR code to visit Frontend Mentor and take your coding skills to
+      the next level
+    </p>
+  </div>
+</template>
