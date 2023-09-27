7d5001757c028709657ff6740978f0c581ad54a6
Anne <anneris.wong@gmail.com>
2023-09-26 22:19:38 -0400

    Updating styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        libre: ['"Outfit"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
